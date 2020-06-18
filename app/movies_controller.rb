10fe6ddb47949382057cf2b3c89de39823022c01
Agi Letkiewicz <a.letkiewicz@gmail.com>
2020-06-18 08:20:39 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -23,7 +23,7 @@ def can_be_created_with_a_hash_of_attributes
   movie = Movie.create(attributes)
 end
 
-def can_be_created_in_a_block(args = {title=> "Home Alone", release_date=> 1990 })
+def can_be_created_in_a_block(args = {title= "Home Alone", release_date= 1990 })
   # If no arguments are passed, use default values:
   # title == "Home Alone"
   # release_date == 1990
@@ -54,7 +54,7 @@ def can_find_by_multiple_attributes
   # title == "Title"
   # release_date == 2000
   # director == "Me"
-  __
+  Movie.find_by
 end
 
 def can_find_using_where_clause_and_be_sorted
