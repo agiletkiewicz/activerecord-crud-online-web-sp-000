6dccd51d5a590fe00541aa63d0f4731e592c7819
Agi Letkiewicz <a.letkiewicz@gmail.com>
2020-06-18 08:26:56 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -74,8 +74,8 @@ end
 def can_update_using_update_method
   # Update movie title to "Wat, huh?"
   Movie.create(title: "Wat?")
-  movie = 
-  __
+  movie = Movie.find_by(title: "Wat?")
+  movie.update(title: "WAt, huh?")
 end
 
 def can_update_multiple_items_at_once
