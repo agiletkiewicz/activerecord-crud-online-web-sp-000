5411d59f53c4dde996f98ae3455480e2950405ab
Agi Letkiewicz <a.letkiewicz@gmail.com>
2020-06-18 08:36:50 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -29,7 +29,7 @@ def can_be_created_in_a_block(title= "Home Alone", release_date= 1990)
   # release_date == 1990
   
   Movie.create do |m|
-    m.each {|key, value| self.send(("#{key}="), value)}
+   
   end
 end
 
